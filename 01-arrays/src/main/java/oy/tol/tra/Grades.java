3ada1dc696d0eab07e6024a17713eac35b54828b
LouisanaZhangXuanyu <1376952171@qq.com>
2024-03-08 11:07:19 +0800

    commit

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,73 +1,45 @@
 package oy.tol.tra;
 
-/**
- * A simple array of student grades to be used in testing
- * misbehaving algorithm for reversing the array.
- */
-public class Grades {
-   
-   private Integer [] grades = null;
+public class Grades<T extends Comparable<T>> {
 
-   /**
-    * A constructor for building IntArrays.
-    * @param grades the plain Java integer array with numbers to add.
-    */
-   public Grades(Integer [] grades) {
-      this.grades = new Integer [grades.length];
-      for (int counter = 0; counter < grades.length; counter++) {
-         this.grades[counter] = grades[counter];
-      }
+   private T[] grades = null;
+   public Grades(T[] grades) {
+      this.grades = grades.clone();
    }
-
    /**
-    * The method to reverse the internal Java int array.
+    * 
     */
    public void reverse() {
-      /* TODO:
-       1. Edit the test data files to see if the reverse() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
-      */
-      int i = 0;
-      while (i <= grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
-   }
+      int start = 0;
+      int end = grades.length - 1;
 
-   /**
-    * Sorts the array to ascending order.
-    */
-   public void sort() {
-      /* TODO:
-       1. Edit the test data files to see if the sort() really works or not.
-       2. Execute the IntArrayTests to see that some of them fail.
-       3. Study the code below and try to find what is the issue.
-       4. Use the debugger to see the execution and variable values if necessary.
-       5. Fix the issue.
-       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
-      */
-      int i = grades.length-1;
-      while (i > 0) {
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
-         }
-         i--;
+      while (start < end) {
+         T temp = grades[start];
+         grades[start] = grades[end];
+         grades[end] = temp;
+         start++;
+         end--;
       }
    }
 
    /**
-    * Returns the plain Java int [] array for investigation.
-    * @return The int array.
+    * 
     */
-   public Integer [] getArray() {
-      return grades;
+   public void sort() {
+      boolean swapped=true;
+      while (swapped==true){
+         swapped = false;
+         for (int i = 1; i < grades.length; i++) {
+            if (grades[i - 1].compareTo(grades[i]) > 0) {
+               T temp = grades[i - 1];
+               grades[i - 1] = grades[i];
+               grades[i] = temp;
+               swapped = true;
+            }
+         }
+      }
+   }
+   public T[] getArray() {
+      return grades.clone();
    }
 }
