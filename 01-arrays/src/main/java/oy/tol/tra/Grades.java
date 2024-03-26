5c96c0a202d420e807bd1986b20edc8d992b1e66
Xuanyu Zhang <126142147+LouisanaZhangXuanyu@users.noreply.github.com>
2024-03-26 21:30:47 +0800

    Grades.java

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,53 +1,20 @@
 package oy.tol.tra;
-
-public class Grades<T extends Comparable<T>> {
-
-   private T[] grades = null;
-   public Grades(T[] grades) {
-      this.grades = grades.clone();
+public class Grades {
+   
+   private Integer [] grades = null;
+   public Grades(Integer [] grades) {
+      this.grades = new Integer [grades.length];
+      for (int counter = 0; counter < grades.length; counter++) {
+         this.grades[counter] = grades[counter];
+      }
    }
-   /**
-<<<<<<< HEAD
-    * 
-=======
-    *
->>>>>>> 47434adae5c33ba265ca4c812421097df94ed50b
-    */
    public void reverse() {
-      int start = 0;
-      int end = grades.length - 1;
-
-      while (start < end) {
-         T temp = grades[start];
-         grades[start] = grades[end];
-         grades[end] = temp;
-         start++;
-         end--;
-      }
+   Algorithms.reverse(grades);
    }
-
-   /**
-<<<<<<< HEAD
-    * 
-=======
-    *
->>>>>>> 47434adae5c33ba265ca4c812421097df94ed50b
-    */
    public void sort() {
-      boolean swapped=true;
-      while (swapped==true){
-         swapped = false;
-         for (int i = 1; i < grades.length; i++) {
-            if (grades[i - 1].compareTo(grades[i]) > 0) {
-               T temp = grades[i - 1];
-               grades[i - 1] = grades[i];
-               grades[i] = temp;
-               swapped = true;
-            }
-         }
-      }
+      Algorithms.sort(grades);
    }
-   public T[] getArray() {
-      return grades.clone();
+   public Integer [] getArray() {
+      return grades;
    }
 }
