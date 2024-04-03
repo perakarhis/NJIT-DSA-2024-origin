93a98936b9ffc00094ca5561a7f9987112aefef4
Xuanyu Zhang <126142147+LouisanaZhangXuanyu@users.noreply.github.com>
2024-04-03 18:11:46 +0800

    Update KeyValueBSearchTree.java

--- 04-phonebook/src/main/java/oy/tol/tra/KeyValueBSearchTree.java
+++ 04-phonebook/src/main/java/oy/tol/tra/KeyValueBSearchTree.java
@@ -1,15 +1,7 @@
 package oy.tol.tra;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 public class KeyValueBSearchTree<K extends Comparable<K>, V> implements Dictionary<K, V> {
-
-    // This is the BST implementation, KeyValueHashTable has the hash table
-    // implementation
-
-    private TreeNode<K, V> root=null;
+    private TreeNode<K, V> root;
     private int count = 0;
     private int maxTreeDepth = 0;
 
@@ -20,60 +12,69 @@ public class KeyValueBSearchTree<K extends Comparable<K>, V> implements Dictiona
 
     @Override
     public int size() {
+        // TODO: Implement this
         return count;
     }
-
-   @Override
+    @Override
     public String getStatus() {
-        StringBuilder toReturn1=new StringBuilder("Tree has max depth of " + maxTreeDepth + ".\n");
-        toReturn1.append("Longest collision chain in a tree node is " + TreeNode.longestCollisionChain + "\n");
+        String toReturn = "Tree has max depth of " + maxTreeDepth + ".\n";
+        toReturn += "Longest collision chain in a tree node is " + TreeNode.longestCollisionChain + "\n";
         TreeAnalyzerVisitor<K, V> visitor = new TreeAnalyzerVisitor<>();
         root.accept(visitor);
-        toReturn1.append("Min path height to bottom: " + visitor.minHeight + "\n");
-        toReturn1.append("Max path height to bottom: " + visitor.maxHeight + "\n");
-        toReturn1.append("Ideal height if balanced: " + Math.ceil(Math.log(count)) + "\n");
-        String toReturn = toReturn1.toString();
+        toReturn += "Min path height to bottom: " + visitor.minHeight + "\n";
+        toReturn += "Max path height to bottom: " + visitor.maxHeight + "\n";
+        toReturn += "Ideal height if balanced: " + Math.ceil(Math.log(count)) + "\n";
         return toReturn;
     }
 
+    @Override
     public boolean add(K key, V value) throws IllegalArgumentException, OutOfMemoryError {
-        if(key==null||value==null)
-        {
-            throw new IllegalArgumentException("This is a custom NullPointerException message");
+        if(key == null || value == null){
+            throw new IllegalArgumentException("Cannot be null!");
         }
-        if(root==null)
-        {
-            root=new TreeNode<K,V>(key, value);
-            count++;
-            return true;
+        // If root is null, should go there.
+        TreeNode<K, V> newNode = new TreeNode<>(key, value);
+            if(root == null){
+                root = newNode;  
+                count++;  
+                maxTreeDepth = 1;
+                return true;
+            }
+        int newOne = root.insert(key, value, key.hashCode());
+        if (newOne == 1) {  
+            if(TreeNode.currentAddTreeDepth > maxTreeDepth){
+                maxTreeDepth = TreeNode.currentAddTreeDepth;
+            }
+            TreeNode.currentAddTreeDepth = 0;
+            count++; 
+        } 
+        return true;
+    }   
+
+    @Override
+    public V find(K key) throws IllegalArgumentException {
+       
+        if(key == null){
+            throw new IllegalArgumentException("Cannot be null!");
         }
-        else {
-           count+= root.insert(key,value,key.hashCode());
-           return true;
-        }
-        }
-   
+        return (root.find(key,key.hashCode()));
+    }
+
     @Override
     public void ensureCapacity(int size) throws OutOfMemoryError {
-        // Nothing to do here. Trees need no capacity.
+
     }
-  
-public Pair<K, V>[] toSortedArray() {
-    TreeToArrayVisitor<K, V> visitor = new TreeToArrayVisitor<>(count);
-    root.accept(visitor);
-    Pair<K, V>[] sorted = visitor.getArray();
-    Algorithms.mergeSort(sorted);
-    return sorted;
-}
-
-public V find(K key) {
-    return root.find(key, key.hashCode());
-}
-
-
 
     @Override
-    public void compress() throws OutOfMemoryError {
+    public Pair<K, V>[] toSortedArray() {
+        TreeToArrayVisitor<K, V> visitor = new TreeToArrayVisitor<>(count);
+        root.accept(visitor);
+        Pair<K, V>[] sorted = visitor.getArray();
+        Algorithms.fastSort(sorted);
+        return sorted;
     }
-    
+
+    @Override
+    public void compress() throws OutOfMemoryError {  
+    }
 }
